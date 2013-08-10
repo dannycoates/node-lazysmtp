1ad7f465229da22d74dbebcacdd3605d75d44c90
Danny Coates <dannycoates@gmail.com>
2013-08-09 19:25:08 -0700

    send Ok to EHLO

--- index.js
+++ index.js
@@ -4,54 +4,54 @@ var util          = require("util");
 
 var Mail    = function (host, debug) {
     EventEmitter.call(this);
-    
+
     this.debug      = debug;
     this.host       = host;
     var that        = this;
 
     var server      = net.createServer(function (socket)     {
-        
+
         var data        = "";
         var moreData    = false;
         var client      = socket.remoteAddress + ":" + socket.remotePort;
-        
+
         //Start handshake
         log(that.debug, "Client connected:" + client);
-        
+
         that.emit("connectionIncoming", { client: { address: socket.remoteAddress, port: socket.remotePort }});
 
         replies.connect(socket);
-        
+
         socket.on("data", function(d) {
-            
+
             log(that.debug, "Received data from client:" + client);
             log(that.debug, d.toString());
             log(that.debug, "End of data from client:" + client);
 
             //In case we expect more data, for example when an e-mail is very large.
-                
+
             if(moreData === true) {
-            
+
                 data += d.toString();
-            
+
             } else {
 
                 data = d.toString();
-            
+
             }
-            
+
             moreData = false;
-            
+
             //What follows is a range of steps from the SMTP handshake.
 
             if(/^EHLO/i.test(data)) {
 
                 replies.ehlo(socket);
-        
+
             } else if(/^HELO/i.test(data)) {
-              
-                replies.helo(socket);  
-                
+
+                replies.helo(socket);
+
             } else if(/^MAIL FROM/i.test(data)) {
 
                 replies.mailFrom(socket);
@@ -61,20 +61,20 @@ var Mail    = function (host, debug) {
                 replies.recipient(socket);
 
             } else if(/^DATA/i.test(data)) {
-            
+
                 replies.data(socket);
 
             } else if(/^QUIT/i.test(data)) {
-          
-                replies.quit(socket);  
-            
+
+                replies.quit(socket);
+
             } else if(/^.\r\n/m.test(data)) {
 
                 replies.end(socket, data);
-        
+
             } else {
-               
-                //Assume we're receiving more data and that we're not ready to parse yet. 
+
+                //Assume we're receiving more data and that we're not ready to parse yet.
                 moreData = true;
 
             }
@@ -82,7 +82,7 @@ var Mail    = function (host, debug) {
         });
 
         socket.on("close", function() {
-            
+
             that.emit("connectionClosed", { client: { address: socket.remoteAddress, port: socket.remotePort }} );
             log(that.debug, "Connection closed");
 
@@ -91,8 +91,8 @@ var Mail    = function (host, debug) {
     });
 
     server.on("error", function(error) {
-        
-        that.emit("error", error);    
+
+        that.emit("error", error);
         log(that.debug, "Something went wrong:" + error.code);
 
     });
@@ -103,7 +103,7 @@ var Mail    = function (host, debug) {
 
     replies.connect = function (socket) {
 
-        send( socket, "220 " + host );     
+        send( socket, "220 " + host );
 
     }
 
@@ -133,10 +133,10 @@ var Mail    = function (host, debug) {
     }
 
     replies.ehlo    = function (socket) {
-    
+
         send(socket, "250-" + host );
         send(socket, "250 8BITMIME");
-
+        send(socket, "250 Ok");
     }
 
     replies.mailFrom    = function (socket) {
@@ -146,20 +146,20 @@ var Mail    = function (host, debug) {
     }
 
     replies.recipient   = function (socket) {
-      
+
         send(socket, "250 Ok");
 
     }
 
     function send (socket, message) {
-        
+
         log(that.debug, "sending:" + message);
         socket.write(message + "\r\n");
 
     };
 
     this.start = function (port) {
-        
+
         log(that.debug, "Starting lazysmtp");
         server.listen(port || 25);
 
@@ -171,11 +171,11 @@ util.inherits(Mail, EventEmitter);
 
 
 function log(debug, message) {
-    
+
     if(debug) {
 
         util.log(message);
-    
+
     }
 
 }
